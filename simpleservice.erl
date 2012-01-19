139c91021e919a3eb3f1c6c46169f134bfa75cd7
Ian Brown <igb@Ian-Browns-MacBook-Pro.local>
2012-01-18 23:26:38 -0800

    initial rev of simple service wrapper

---
+++ simpleservice.erl
@@ -0,0 +1,62 @@
+-module(simpleservice).
+-export([start/2, loop/2, handle_request/2]).
+
+start(Port, Functions)->    
+    {ok, ListenSock}=gen_tcp:listen(Port, [list,{active, false},{packet,http}]),
+    ?MODULE:loop(ListenSock, Functions).
+
+loop(ListenSock, Functions) ->
+    
+    {ok, Sock}=gen_tcp:accept(ListenSock),
+    spawn(?MODULE, handle_request, [Sock, Functions]),
+    ?MODULE:loop(ListenSock, Functions).
+
+
+handle_request(Sock, Functions) ->
+    {ok, {http_request, Method, Path, Version}}=gen_tcp:recv(Sock, 0),
+    {Headers, Body}=marshall_request(Sock, Method),
+   
+    MethodAtom=convert_method_to_atom(Method),
+    Function=get_function(MethodAtom, Functions),
+    Function(Headers, Body, Sock),
+    gen_tcp:close(Sock).
+
+get_function(MethodAtom, [Head|Tail])->
+    case Head of
+	    {MethodAtom, Function}->
+			 Function;
+		_ ->get_function(MethodAtom, Tail)
+		 end;
+get_function(MethodAtom, []) ->
+    err.
+
+marshall_request(Sock, Method) ->
+	 headers(Sock, Method, []).
+	
+headers(Sock, Method, Headers) ->
+	case gen_tcp:recv(Sock, 0) of	
+	    {ok, http_eoh} -> body(Sock, Method, Headers);
+	    {ok, {http_header,_,HeaderName,_, HeaderValue}} -> io:fwrite("~s: ~s~n", [HeaderName, HeaderValue]), headers(Sock, Method, lists:append([{HeaderName, HeaderValue}], Headers))
+	end.
+
+body(Sock, Method, Headers) ->
+    case (Method) of 
+	'GET' -> {Headers, []};
+	'POST' -> read_body(Sock, Headers);
+	'PUT' ->read_body(Sock, Headers);
+	'DELETE' -> {Headers, []}
+
+	end.
+
+read_body(Sock, Headers)->
+    inet:setopts(Sock, [{packet, raw}]),
+    case gen_tcp:recv(Sock, 0) of
+	{ok, Body} -> {Headers, Body};
+	_ -> {error, "We No Habeas Corpus!"}
+    end.
+
+convert_method_to_atom(Method)->
+    case (Method) of 
+		'GET' -> get;
+		'POST' -> post
+    end.
